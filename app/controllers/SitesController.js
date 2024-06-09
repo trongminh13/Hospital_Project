111fcf98e18ab1f5bfb55d641086283930fa9dff
= <=>
2024-06-10 00:12:05 +0700

    fix bug không render được database benhvien.json

--- app/controllers/SitesController.js
+++ app/controllers/SitesController.js
@@ -1,54 +1,28 @@
 const Course = require('../models/course');
 const { mutipleMongooseToObject } = require('../../util/mongoose');
+const fs = require('fs');
+const path = require('path');
 
 class SitesController{
-  index(req,res,next){
-    Course.find({})
-      .then(courses =>{
-       
-        res.render('home',{ 
-          courses: mutipleMongooseToObject(courses)
-         });
-      
-    })
-    
-      .catch(next);
-    }
+  index(req, res, next) {
+    fs.readFile(path.join(__dirname, '..', 'Database', 'benhvien.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading benhvien.json:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
 
+        const courses = JSON.parse(data);
+        res.render('home', { courses: courses });
+    });
+}
      
     search(req, res) {
     res.render('search');
   }
 }
 
-
 module.exports = new SitesController();
-
-
-
-
-
-  // async index(req, res) {
-
-  //   try {
-
-  //       const data = await Course.find({});
-
-  //       res.json(data);
-
-  //   }  catch (err) {
-
-  //       res.status(400).json({error: err});
-  //   }
-    
-  // }
-
-
-
-// res.json({
-    //   name: 'test'
-    // })
-  
 
 
 
